bc2eaa8efb231b1c455de2b2fbf2b137f98f8502
Kartik Mahadev Vhonale <98228313+KartikVhonale@users.noreply.github.com>
2024-05-17 12:56:09 +0530

    Add files via upload

--- BuySell.java
+++ BuySell.java
@@ -35,4 +35,12 @@ public class BuySell {
         
         return maxi;
     }
+    public int maxProfit(int[] prices) {
+        int buy=prices[0], profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>buy) profit+=(prices[i]-buy);
+            buy=prices[i];
+        }
+        return profit;
+    }
 }
